7404a94fbdbd074810a8d8b66229fe16ed6e7518
roy terrell <rmt2bsc@gmail.com>
2017-02-25 12:08:51 -0600

    Created

---
+++ src/main/java/org/rmt2/rest/PublicGatewayServerApplication.java
@@ -0,0 +1,32 @@
+package org.rmt2.rest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+import org.rmt2.rest.accounting.AccountingEntityResource;
+import org.rmt2.rest.addressbook.ContactEntityResource;
+import org.rmt2.rest.media.DocumentMediaResource;
+
+public class PublicGatewayServerApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> classes = new HashSet<Class<?>>();
+        classes.add(AccountingEntityResource.class);
+        classes.add(ContactEntityResource.class);
+        classes.add(DocumentMediaResource.class);
+        return classes;
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        final Set<Object> singletons = new HashSet<Object>();
+
+        // TODO: Add singleton classes
+
+        return singletons;
+    }
+
+}
