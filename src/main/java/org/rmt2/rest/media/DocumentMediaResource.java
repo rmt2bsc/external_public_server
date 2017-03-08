305066f1743d28a2e21c36a434d29fa73f1679da
appdev <royterrell2@gmail.com>
2017-03-08 03:16:59 -0600

    Added validation logic

--- src/main/java/org/rmt2/rest/media/DocumentMediaResource.java
+++ src/main/java/org/rmt2/rest/media/DocumentMediaResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -19,9 +20,9 @@ import org.rmt2.jaxb.MultimediaResponse;
 import org.rmt2.jaxb.ObjectFactory;
 import org.rmt2.rest.RMT2BaseRestResouce;
 
+import com.api.messaging.MessageRoutingException;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.util.RMT2File;
 
 @Path("/media/document/{transaction}")
 public class DocumentMediaResource extends RMT2BaseRestResouce {
@@ -35,7 +36,8 @@ public class DocumentMediaResource extends RMT2BaseRestResouce {
      * Fetch the metadata and binary content by content id.
      * 
      * @param contentId
-     *            the unique identifier of the content record.
+     *            the unique identifier of the content record. Must be greater
+     *            than zero.
      * @return JSON containing metadata and base64 encoded binary content
      */
     @GET
@@ -43,32 +45,47 @@ public class DocumentMediaResource extends RMT2BaseRestResouce {
     @Produces(MediaType.APPLICATION_JSON)
     public Response fetchImageContent(@PathParam("contentId") final long contentId) {
         LOGGER.info("REST method, forms/{contentId}/image-attachment, was called");
-        ObjectFactory f = new ObjectFactory();
+        if (contentId <= 0) {
+            this.msg = "Web Service Error: Content Id must be greater than zero in order to retrieve image attachment";
+            LOGGER.error(this.msg);
+            throw new WebApplicationException(Response.status(Status.BAD_REQUEST).type(MediaType.TEXT_PLAIN_TYPE)
+                    .entity(this.msg).build());
+        }
 
         // Create multimedia request object with "contentId" param
+        ObjectFactory f = new ObjectFactory();
         MultimediaRequest req = f.createMultimediaRequest();
         req.setHeader(this.getHeader());
         req.setContentId(BigInteger.valueOf(contentId));
 
         // Route message to business server
-        Object response = this.msgRouterHelper.routeJsonMessage(this.transaction, req);
-
         MultimediaResponse r = null;
-        if (response != null && response instanceof MultimediaResponse) {
-            r = (MultimediaResponse) response;
+        try {
+            Object response = this.msgRouterHelper.routeJsonMessage(this.transaction, req);
+            if (response != null && response instanceof MultimediaResponse) {
+                r = (MultimediaResponse) response;
+            }
+        } catch (MessageRoutingException e) {
+            this.msg = e.getMessage();
+            LOGGER.error("Unable to route attachment/{contentId} to its destination", e);
+            throw new WebApplicationException(Response.status(Status.INTERNAL_SERVER_ERROR)
+                    .type(MediaType.TEXT_PLAIN_TYPE).entity(this.msg).build());
         }
-        else { // TODO: Remove "else" once properly implemented
-            r = f.createMultimediaResponse();
-            MimeContentType content = f.createMimeContentType();
-            content.setAppCode("ACCT");
-            content.setContentId(BigInteger.valueOf(contentId));
-            content.setFilename("example.jpg");
-            content.setFilepath("/tmp/somefilepath/");
-            String imgContent = RMT2File
-                    .getFileContentAsBase64("/Users/royterrell/Pictures/pearl-weathered-leather-1600-1200.jpg");
-            content.setBinaryData(imgContent);
-            r.setContent(content);
-        }
+        // if (response != null && response instanceof MultimediaResponse) {
+        // r = (MultimediaResponse) response;
+        // }
+        // else { // TODO: Remove "else" once properly implemented
+        // r = f.createMultimediaResponse();
+        // MimeContentType content = f.createMimeContentType();
+        // content.setAppCode("ACCT");
+        // content.setContentId(BigInteger.valueOf(contentId));
+        // content.setFilename("example.jpg");
+        // content.setFilepath("/tmp/somefilepath/");
+        // String imgContent = RMT2File
+        // .getFileContentAsBase64("/Users/royterrell/Pictures/pearl-weathered-leather-1600-1200.jpg");
+        // content.setBinaryData(imgContent);
+        // r.setContent(content);
+        // }
 
         // Marshal to JSON
         final Gson gson = new GsonBuilder().create();
