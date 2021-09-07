77b3c9513926c646de25cd93b0e50471e1678020
appdev <royterrell2@gmail.com>
2021-09-07 14:15:44 -0500

    IS-65:  Created accounting subsidiary unit test cases.

---
+++ src/test/java/org/rmt2/soap/accounting/subsidiary/CustomerQuerySoapRequestBuilderTest.java
@@ -0,0 +1,104 @@
+package org.rmt2.soap.accounting.subsidiary;
+
+import java.util.Date;
+
+import javax.xml.soap.SOAPMessage;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.rmt2.constants.ApiHeaderNames;
+import org.rmt2.constants.ApiTransactionCodes;
+import org.rmt2.constants.MessagingConstants;
+import org.rmt2.jaxb.AccountingTransactionRequest;
+import org.rmt2.jaxb.BusinessType;
+import org.rmt2.jaxb.CustomerCriteriaType;
+import org.rmt2.jaxb.CustomerType;
+import org.rmt2.jaxb.HeaderType;
+import org.rmt2.jaxb.ObjectFactory;
+import org.rmt2.jaxb.TransactionCriteriaGroup;
+import org.rmt2.jaxb.XactCustomCriteriaTargetType;
+import org.rmt2.util.HeaderTypeBuilder;
+import org.rmt2.util.accounting.subsidiary.CustomerTypeBuilder;
+import org.rmt2.util.addressbook.BusinessTypeBuilder;
+
+import com.api.config.ConfigConstants;
+import com.api.config.SystemConfigurator;
+import com.api.messaging.webservice.soap.SoapMessageHelper;
+import com.api.xml.jaxb.JaxbUtil;
+
+public class CustomerQuerySoapRequestBuilderTest {
+
+    private JaxbUtil jaxb;
+    
+    @Before
+    public void setUp() throws Exception {
+        try {
+            jaxb = SystemConfigurator.getJaxb(ConfigConstants.JAXB_CONTEXNAME_DEFAULT);
+        }
+        catch (Exception e) {
+            jaxb = new JaxbUtil(MessagingConstants.JAXB_RMT2_PKG);
+        }
+    }
+    
+    @Test
+    public void testBuildRequest() {
+        ObjectFactory fact = new ObjectFactory();
+        AccountingTransactionRequest req = fact.createAccountingTransactionRequest();
+        
+        HeaderType head =  HeaderTypeBuilder.Builder.create()
+                .withApplication("accounting")
+                .withModule("subsidiary")
+                .withMessageMode(ApiHeaderNames.MESSAGE_MODE_REQUEST)
+                .withDeliveryDate(new Date())
+                
+                // Set these header elements with dummy values in order to be properly assigned later.
+                .withTransaction(ApiTransactionCodes.SUBSIDIARY_CUSTOMER_GET)
+                .withRouting(ApiHeaderNames.DUMMY_HEADER_VALUE)
+                .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE).build();
+        
+        BusinessType busType = BusinessTypeBuilder.Builder.create()
+                .withBusinessId(1351)
+                .withLongname("Business Type Description").build();
+        
+        CustomerType custType = CustomerTypeBuilder.Builder.create()
+                .withCustomerId(3333)
+                .withAcctId(1234567)
+                .withPersonType(null)
+                .withAccountNo("ACCT-NO-8888")
+                .withCreditLimit(1234.55)
+                .withAcctDescription("ACCOUNT DESCRIPTION")
+                .withBalance(50000)
+                .withActive(1).build();
+                
+        CustomerCriteriaType criteria = fact.createCustomerCriteriaType();
+        criteria.setTargetLevel(XactCustomCriteriaTargetType.FULL);
+        
+        criteria.setCustomer(custType);
+        criteria.setContactDetails(busType);
+
+        TransactionCriteriaGroup criteriaGroup = fact.createTransactionCriteriaGroup();
+        criteriaGroup.setCustomerCriteria(criteria);
+        req.setCriteria(criteriaGroup);
+        req.setHeader(head);
+        
+        // Create SOAP object using response XML
+        String bodyXml = jaxb.marshalJsonMessage(req);
+        Assert.assertNotNull(bodyXml);
+        Assert.assertTrue(bodyXml.contains(ApiTransactionCodes.SUBSIDIARY_CUSTOMER_GET));
+        SoapMessageHelper util = new SoapMessageHelper();
+        String soapXml = util.createRequest(bodyXml);
+        Assert.assertNotNull(soapXml);
+        Assert.assertTrue(soapXml.contains(ApiTransactionCodes.SUBSIDIARY_CUSTOMER_GET));
+        SOAPMessage soapObj = util.getSoapInstance(soapXml);
+        Assert.assertNotNull(soapObj);
+
+        // Extract Body from SOAP object
+        bodyXml = util.getBody(soapObj);
+        Assert.assertNotNull(bodyXml);
+        Assert.assertTrue(bodyXml.contains(ApiTransactionCodes.SUBSIDIARY_CUSTOMER_GET));
+        System.out.println("XML extracted from SOAP body instance:  ");
+        System.out.println(bodyXml);
+    }
+
+}
