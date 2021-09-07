31e3cce205e2b7fa1ae51d24e031a48c02d0f27c
appdev <royterrell2@gmail.com>
2021-09-07 14:29:01 -0500

    IS-65:  Created addressbook lookup unit test cases

---
+++ src/test/java/org/rmt2/soap/addressbook/lookup/LookupCodeUpdateSoapRequestBuilderTest.java
@@ -0,0 +1,82 @@
+package org.rmt2.soap.addressbook.lookup;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+import javax.xml.soap.SOAPMessage;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.rmt2.constants.ApiHeaderNames;
+import org.rmt2.constants.ApiTransactionCodes;
+import org.rmt2.constants.MessagingConstants;
+import org.rmt2.jaxb.CodeDetailType;
+import org.rmt2.jaxb.HeaderType;
+import org.rmt2.jaxb.LookupCodesRequest;
+import org.rmt2.jaxb.ObjectFactory;
+import org.rmt2.util.HeaderTypeBuilder;
+
+import com.api.config.ConfigConstants;
+import com.api.config.SystemConfigurator;
+import com.api.messaging.webservice.soap.SoapMessageHelper;
+import com.api.xml.jaxb.JaxbUtil;
+
+public class LookupCodeUpdateSoapRequestBuilderTest {
+
+    private JaxbUtil jaxb;
+    
+    @Before
+    public void setUp() throws Exception {
+        try {
+            jaxb = SystemConfigurator.getJaxb(ConfigConstants.JAXB_CONTEXNAME_DEFAULT);
+        }
+        catch (Exception e) {
+            jaxb = new JaxbUtil(MessagingConstants.JAXB_RMT2_PKG);
+        }
+    }
+
+    @Test
+    public void testBuildRequest() {
+        ObjectFactory fact = new ObjectFactory();
+        LookupCodesRequest req = fact.createLookupCodesRequest();
+        
+        HeaderType head =  HeaderTypeBuilder.Builder.create()
+                .withApplication("addressbook")
+                .withModule(ConfigConstants.API_APP_MODULE_VALUE)
+                .withMessageMode(ApiHeaderNames.MESSAGE_MODE_REQUEST)
+                .withDeliveryDate(new Date())
+                
+                // Set these header elements with dummy values in order to be properly assigned later.
+                .withTransaction(ApiTransactionCodes.LOOKUP_CODE_UPDATE)
+                .withRouting(ApiHeaderNames.DUMMY_HEADER_VALUE)
+                .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE).build();
+        
+        CodeDetailType cdt = fact.createCodeDetailType();
+        cdt.setCodeId(BigInteger.valueOf(200));
+        cdt.setGroupId(BigInteger.valueOf(100));
+        cdt.setLongdesc("Code detail long description Test");
+        cdt.setShortdesc("Code detail short description Test");
+        cdt.setLabel("Label Test");
+        req.getDetailCodes().add(cdt);
+        req.setHeader(head);
+        
+        // Create SOAP object using response XML
+        String bodyXml = jaxb.marshalJsonMessage(req);
+        Assert.assertNotNull(bodyXml);
+        Assert.assertTrue(bodyXml.contains(ApiTransactionCodes.LOOKUP_CODE_UPDATE));
+        SoapMessageHelper util = new SoapMessageHelper();
+        String soapXml = util.createRequest(bodyXml);
+        Assert.assertNotNull(soapXml);
+        Assert.assertTrue(soapXml.contains(ApiTransactionCodes.LOOKUP_CODE_UPDATE));
+        SOAPMessage soapObj = util.getSoapInstance(soapXml);
+        Assert.assertNotNull(soapObj);
+
+        // Extract Body from SOAP object
+        bodyXml = util.getBody(soapObj);
+        Assert.assertNotNull(bodyXml);
+        Assert.assertTrue(bodyXml.contains(ApiTransactionCodes.LOOKUP_CODE_UPDATE));
+        System.out.println("XML extracted from SOAP body instance:  ");
+        System.out.println(bodyXml);
+    }
+}
---
+++ src/test/java/org/rmt2/soap/addressbook/lookup/LookupGroupQuerySoapRequestBuilderTest.java
@@ -0,0 +1,80 @@
+package org.rmt2.soap.addressbook.lookup;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+import javax.xml.soap.SOAPMessage;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.rmt2.constants.ApiHeaderNames;
+import org.rmt2.constants.ApiTransactionCodes;
+import org.rmt2.constants.MessagingConstants;
+import org.rmt2.jaxb.HeaderType;
+import org.rmt2.jaxb.LookupCodeCriteriaType;
+import org.rmt2.jaxb.LookupCodesRequest;
+import org.rmt2.jaxb.ObjectFactory;
+import org.rmt2.util.HeaderTypeBuilder;
+
+import com.api.config.ConfigConstants;
+import com.api.config.SystemConfigurator;
+import com.api.messaging.webservice.soap.SoapMessageHelper;
+import com.api.xml.jaxb.JaxbUtil;
+
+public class LookupGroupQuerySoapRequestBuilderTest {
+
+    private JaxbUtil jaxb;
+    
+    @Before
+    public void setUp() throws Exception {
+        try {
+            jaxb = SystemConfigurator.getJaxb(ConfigConstants.JAXB_CONTEXNAME_DEFAULT);
+        }
+        catch (Exception e) {
+            jaxb = new JaxbUtil(MessagingConstants.JAXB_RMT2_PKG);
+        }
+    }
+    
+    @Test
+    public void testBuildRequest() {
+        ObjectFactory fact = new ObjectFactory();
+        LookupCodesRequest req = fact.createLookupCodesRequest();
+        
+        HeaderType head =  HeaderTypeBuilder.Builder.create()
+                .withApplication("addressbook")
+                .withModule(ConfigConstants.API_APP_MODULE_VALUE)
+                .withMessageMode(ApiHeaderNames.MESSAGE_MODE_REQUEST)
+                .withDeliveryDate(new Date())
+                
+                // Set these header elements with dummy values in order to be properly assigned later.
+                .withTransaction(ApiTransactionCodes.LOOKUP_GROUP_GET)
+                .withRouting(ApiHeaderNames.DUMMY_HEADER_VALUE)
+                .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE).build();
+        
+        LookupCodeCriteriaType criteria = fact.createLookupCodeCriteriaType();
+        criteria.setGroup(BigInteger.valueOf(200));
+        criteria.setGroupDescription("Group Description Test");
+        criteria.setAll(true);
+        req.setCriteria(criteria);
+        req.setHeader(head);
+        
+        // Create SOAP object using response XML
+        String bodyXml = jaxb.marshalJsonMessage(req);
+        Assert.assertNotNull(bodyXml);
+        Assert.assertTrue(bodyXml.contains(ApiTransactionCodes.LOOKUP_GROUP_GET));
+        SoapMessageHelper util = new SoapMessageHelper();
+        String soapXml = util.createRequest(bodyXml);
+        Assert.assertNotNull(soapXml);
+        Assert.assertTrue(soapXml.contains(ApiTransactionCodes.LOOKUP_GROUP_GET));
+        SOAPMessage soapObj = util.getSoapInstance(soapXml);
+        Assert.assertNotNull(soapObj);
+
+        // Extract Body from SOAP object
+        bodyXml = util.getBody(soapObj);
+        Assert.assertNotNull(bodyXml);
+        Assert.assertTrue(bodyXml.contains(ApiTransactionCodes.LOOKUP_GROUP_GET));
+        System.out.println("XML extracted from SOAP body instance:  ");
+        System.out.println(bodyXml);
+    }
+}
