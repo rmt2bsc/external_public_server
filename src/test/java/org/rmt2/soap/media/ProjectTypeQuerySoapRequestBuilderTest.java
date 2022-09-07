8ab7aa8ffbe17bd6557b77f4932e6d4de9d10f20
appdev <royterrell2@gmail.com>
2022-09-06 23:25:17 -0500

    IS-70:  Developed message handler test cases for transaction code, GET_PROJECT_TYPE.

--- src/test/java/org/rmt2/soap/media/ProjectTypeQuerySoapRequestBuilderTest.java
+++ src/test/java/org/rmt2/soap/media/ProjectTypeQuerySoapRequestBuilderTest.java
@@ -1,6 +1,5 @@
 package org.rmt2.soap.media;
 
-import java.math.BigInteger;
 import java.util.Date;
 
 import javax.xml.soap.SOAPMessage;
@@ -11,10 +10,10 @@ import org.junit.Test;
 import org.rmt2.constants.ApiHeaderNames;
 import org.rmt2.constants.ApiTransactionCodes;
 import org.rmt2.constants.MessagingConstants;
-import org.rmt2.jaxb.CodeGroupType;
+import org.rmt2.jaxb.AudioVideoCriteriaType;
 import org.rmt2.jaxb.HeaderType;
-import org.rmt2.jaxb.LookupCodesRequest;
 import org.rmt2.jaxb.MimeDetailGroup;
+import org.rmt2.jaxb.MultimediaRequest;
 import org.rmt2.jaxb.MultimediaResponse;
 import org.rmt2.jaxb.ObjectFactory;
 import org.rmt2.jaxb.ProjectTypes;
@@ -44,7 +43,7 @@ public class ProjectTypeQuerySoapRequestBuilderTest {
     @Test
     public void testBuildRequest() {
         ObjectFactory fact = new ObjectFactory();
-        LookupCodesRequest req = fact.createLookupCodesRequest();
+        MultimediaRequest req = fact.createMultimediaRequest();
         
         HeaderType head =  HeaderTypeBuilder.Builder.create()
                 .withApplication("media")
@@ -53,32 +52,34 @@ public class ProjectTypeQuerySoapRequestBuilderTest {
                 .withDeliveryDate(new Date())
                 
                 // Set these header elements with dummy values in order to be properly assigned later.
-                .withTransaction(ApiTransactionCodes.LOOKUP_GROUP_UPDATE)
+                .withTransaction(ApiTransactionCodes.MEDIA_PROJECTTYPE_GET)
                 .withRouting(ApiTransactionCodes.ROUTE_MULTIMEDIA)
                 .withSessionId(ConfigConstants.API_DUMMY_SESSION_ID)
                 .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE).build();
         
-        CodeGroupType cgt = fact.createCodeGroupType();
-        cgt.setGroupId(BigInteger.valueOf(200));
-        cgt.setGroupDesc("Group Description Test");
-        req.getGroupCodes().add(cgt);
+        AudioVideoCriteriaType cgt = fact.createAudioVideoCriteriaType();
+        cgt.setProjectTypeId(200);
+        cgt.setProjectTypeName("Project Type Description Test");
+
+        req.setCriteria(fact.createMimeCriteriaGroup());
+        req.getCriteria().setAudioVideoCriteria(cgt);
         req.setHeader(head);
         
         // Create SOAP object using response XML
         String bodyXml = jaxb.marshalJsonMessage(req);
         Assert.assertNotNull(bodyXml);
-        Assert.assertTrue(bodyXml.contains(ApiTransactionCodes.LOOKUP_GROUP_UPDATE));
+        Assert.assertTrue(bodyXml.contains(ApiTransactionCodes.MEDIA_PROJECTTYPE_GET));
         SoapMessageHelper util = new SoapMessageHelper();
         String soapXml = util.createRequest(bodyXml);
         Assert.assertNotNull(soapXml);
-        Assert.assertTrue(soapXml.contains(ApiTransactionCodes.LOOKUP_GROUP_UPDATE));
+        Assert.assertTrue(soapXml.contains(ApiTransactionCodes.MEDIA_PROJECTTYPE_GET));
         SOAPMessage soapObj = util.getSoapInstance(soapXml);
         Assert.assertNotNull(soapObj);
 
         // Extract Body from SOAP object
         bodyXml = util.getBody(soapObj);
         Assert.assertNotNull(bodyXml);
-        Assert.assertTrue(bodyXml.contains(ApiTransactionCodes.LOOKUP_GROUP_UPDATE));
+        Assert.assertTrue(bodyXml.contains(ApiTransactionCodes.MEDIA_PROJECTTYPE_GET));
         System.out.println("XML extracted from SOAP body instance:  ");
         System.out.println(bodyXml);
     }
@@ -96,7 +97,7 @@ public class ProjectTypeQuerySoapRequestBuilderTest {
 
                 // Set these header elements with dummy values in order to be
                 // properly assigned later.
-                .withTransaction(ApiTransactionCodes.LOOKUP_GROUP_UPDATE)
+                .withTransaction(ApiTransactionCodes.MEDIA_PROJECTTYPE_GET)
                 .withRouting(ApiTransactionCodes.ROUTE_MULTIMEDIA)
                 .withSessionId(ConfigConstants.API_DUMMY_SESSION_ID)
                 .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE).build();
@@ -124,18 +125,18 @@ public class ProjectTypeQuerySoapRequestBuilderTest {
         // Create SOAP object using response XML
         String bodyXml = jaxb.marshalJsonMessage(req);
         Assert.assertNotNull(bodyXml);
-        Assert.assertTrue(bodyXml.contains(ApiTransactionCodes.LOOKUP_GROUP_UPDATE));
+        Assert.assertTrue(bodyXml.contains(ApiTransactionCodes.MEDIA_PROJECTTYPE_GET));
         SoapMessageHelper util = new SoapMessageHelper();
         String soapXml = util.createRequest(bodyXml);
         Assert.assertNotNull(soapXml);
-        Assert.assertTrue(soapXml.contains(ApiTransactionCodes.LOOKUP_GROUP_UPDATE));
+        Assert.assertTrue(soapXml.contains(ApiTransactionCodes.MEDIA_PROJECTTYPE_GET));
         SOAPMessage soapObj = util.getSoapInstance(soapXml);
         Assert.assertNotNull(soapObj);
 
         // Extract Body from SOAP object
         bodyXml = util.getBody(soapObj);
         Assert.assertNotNull(bodyXml);
-        Assert.assertTrue(bodyXml.contains(ApiTransactionCodes.LOOKUP_GROUP_UPDATE));
+        Assert.assertTrue(bodyXml.contains(ApiTransactionCodes.MEDIA_PROJECTTYPE_GET));
         System.out.println("XML extracted from SOAP body instance:  ");
         System.out.println(bodyXml);
     }
